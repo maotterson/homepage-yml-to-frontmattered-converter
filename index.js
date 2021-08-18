eb2f9b176c6bf077c2071c5b18fa158b67249f00
maotterson <otterson.2@gmail.com>
2021-08-18 16:18:50 -0400

    add conversion script

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const fs = require('fs')
+const inputPath = './yaml'
+const outputPath = './md'
 
 function convertYamls(){
-
+  fs.readdir(inputPath, (err, files) => {
+    files.forEach(file => {
+      fs.readFile((inputPath + '/' + file),'utf-8',(err,content)=>{
+        let mdOutput = "---\n"
+        mdOutput += content + '---'
+        const stream = fs.createWriteStream(outputPath + '/' + file.split('.')[0] + '.md')
+        stream.on('open', () => {
+          stream.write(mdOutput)
+          stream.end()
+        })
+      })
+    })
+  })
 }
 
 convertYamls()
